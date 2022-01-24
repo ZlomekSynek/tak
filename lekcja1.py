400cb11b74da918b5b19c4c16071a558752bf6e4
ZlomekSynek <97887069+ZlomekSynek@users.noreply.github.com>
2022-01-24 15:48:49 +0100

    Add files via upload

--- lekcja1.py
+++ lekcja1.py
@@ -1,37 +1,47 @@
 import pygame
+import random
 
 def main():
     pygame.init()
-    OknoGry=pygame.display.set_mode((400,400),0,32)
+    OknoGry=pygame.display.set_mode((440,440),0,32)
     pygame.display.set_caption("3tieg")
     run=True
     zmienna=100
-    zmienna1=200
-    zmienna2=280
-    zmienna3=180
+    zmienna2=100
     while(run):
         OknoGry.fill((0,0,0))
         pygame.time.delay(100)
         for zdarzenie in pygame.event.get():
             if zdarzenie.type==pygame.QUIT:
                 run=False
-        r=pygame.Rect((80,zmienna),(50,50))
+            elif zdarzenie.type==pygame.KEYDOWN:
+                if zdarzenie.key==pygame.K_LEFT:
+                    zmienna2=zmienna2-20
+                elif zdarzenie.key==pygame.K_RIGHT:
+                    zmienna2=zmienna2+20
+                elif zdarzenie.key==pygame.K_UP:
+                    zmienna=zmienna-20
+                elif zdarzenie.key==pygame.K_DOWN:
+                    zmienna=zmienna+20
+        #tworzenie kwadratu jako weza
+        r=pygame.Rect((zmienna2,zmienna),(20,20))
         pygame.draw.rect(OknoGry,(0,255,0),r)
-        zmienna=zmienna +5
-        r1=pygame.Rect((zmienna3,zmienna1),(50,50))
-        pygame.draw.rect(OknoGry,(0,255,0),r1)
-        zmienna1=zmienna1 +5
-        r2=pygame.Rect((zmienna2,100),(50,50))
-        pygame.draw.rect(OknoGry,(0,255,0),r2)
-        zmienna2=zmienna2 +5
-        if zmienna>440:
-            zmienna=0
-        if zmienna1>440:
-            zmienna1=0
+        #tworzenie jablka za pomoca kola
+        apleX=random.randint(0,21)*20+10
+        apleY=random.randint(0,21)*20+10
+        pygame.draw.circle(OknoGry,(255,0,0),(apleX,apleY),10)
+
+        if zmienna2<0:
+            zmienna2=440
+       
         if zmienna2>440:
             zmienna2=0
-        if zmienna3>440:
-            zmienna3=0
+
+        if zmienna>440:
+            zmienna=0
+
+        if zmienna<0:
+            zmienna=440
         pygame.display.update()
 
 main()
