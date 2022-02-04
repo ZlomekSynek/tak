6da834a1aa9161d80e4633f8dc76e5a382676dda
ZlomekSynek <97887069+ZlomekSynek@users.noreply.github.com>
2022-02-04 08:10:04 +0100

    Add files via upload

--- lekcja1.py
+++ lekcja1.py
@@ -8,6 +8,16 @@ def main():
     run=True
     zmienna=100
     zmienna2=100
+    #losowanie pozycji jablka
+    apleX=random.randint(0,21)*20+10
+    apleY=random.randint(0,21)*20+10
+    #liczenie punktow
+    punkty=0
+    #pozycje weza
+    pozycja=[(zmienna2,zmienna)]
+    #pozycja.append((120,100))
+    dlugoscWeza=1
+
     while(run):
         OknoGry.fill((0,0,0))
         pygame.time.delay(100)
@@ -23,14 +33,39 @@ def main():
                     zmienna=zmienna-20
                 elif zdarzenie.key==pygame.K_DOWN:
                     zmienna=zmienna+20
+                #sprawdzenie czy waz nie zjada siebie
+                for location in pozycja [::]:
+                    if zmienna==location[1] and zmienna2==location[0]:
+                        pozycja=[(zmienna2,zmienna)]
+                        dlugoscWeza=1
+                        punkty=0
+                #dodanie nowej pozycji weza
+                pozycja.append((zmienna2,zmienna))
+                #usuniecie poprzedniej pozycji weza
+                #nie usuwamy pozycji gdy waz zjadl jablko
+                if len(pozycja)>dlugoscWeza:   
+                    del pozycja[0]
         #tworzenie kwadratu jako weza
         r=pygame.Rect((zmienna2,zmienna),(20,20))
         pygame.draw.rect(OknoGry,(0,255,0),r)
+        #rysowanie weza z pozycji
+        for poz in pozycja[::-1]:
+            r=pygame.Rect((poz[0],poz[1]),(20,20))
+            pygame.draw.rect(OknoGry,(0,255,0),r)
         #tworzenie jablka za pomoca kola
-        apleX=random.randint(0,21)*20+10
-        apleY=random.randint(0,21)*20+10
         pygame.draw.circle(OknoGry,(255,0,0),(apleX,apleY),10)
-
+        #sprawdzanie czy waz zjada jablko
+        if((zmienna+10==apleY) and (zmienna2+10==apleX)):
+            dlugoscWeza=dlugoscWeza+1
+            apleX=random.randint(0,21)*20+10
+            apleY=random.randint(0,21)*20+10
+            pygame.draw.circle(OknoGry,(128,128,128),(apleX,apleY),10)
+            #zwiekszenie liczby punktow
+            punkty=punkty+1
+        #wypisanie punktow na ekran
+        czcionka=pygame.font.SysFont('comicsans',30)
+        tekst=czcionka.render("Zdobyłes punkt: {0}".format(punkty),1,(255,160,0))
+        OknoGry.blit(tekst, (10,10))
         if zmienna2<0:
             zmienna2=440
        
