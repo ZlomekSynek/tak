591740c11a188d86f79b23f1e77f78bf73f35040
ZlomekSynek <97887069+ZlomekSynek@users.noreply.github.com>
2022-03-18 08:08:51 +0100

    Add files via upload

--- start.py
+++ start.py
@@ -4,8 +4,13 @@ import lekcja1
 
 def wlaczGre():
     lekcja1.main()
+
 def zmienKolorWaz1(value):
     lekcja1.zmianaKolorWaz1(value)
+def zmienKolorWaz2(value):
+    lekcja1.zmianaKolorWaz2(value)
+def zmienJablka(wartosc,value):
+    lekcja1.iloscJablek=value
 def main():
     pygame.init()
     oknoMenu=pygame.display.set_mode((500,500))
@@ -13,7 +18,9 @@ def main():
     menu=pygame_menu.Menu("Gra Snake 3TieG",500,500,theme=pygame_menu.themes.THEME_DARK)
     menu.add.button("Włącz grę",wlaczGre,background_color=(0,0,255))
     menu.add.color_input('Kolor Wąż 1:','rgb',default=(100,100,255),onreturn=zmienKolorWaz1)
-    menu.add.button('Zapisz kolor Wąż 1')
+    menu.add.color_input('Kolor Wąż 2:','rgb',default=(200,100,255),onreturn=zmienKolorWaz2)
+    menu.add.selector("Wybierz ilość jabłek",[('jedno',1),('dwa',2),('trzy',3),('cztery',4),('pięć',5)],onchange=zmienJablka)
+    #menu.add.button('Zapisz kolor Wąż 1')
     menu.mainloop(oknoMenu)
 
 main()
